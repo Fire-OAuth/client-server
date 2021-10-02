4a4631422ad2b1e4a8233d97f86b0f3a29962d02
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-10-02 23:01:02 +0530

    Added express.static to server static files

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.listen(port, () => console.log("Server Listening on " + port));
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
-
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.status(200).sendFile(path.join(__dirname, "public/login.html"));
