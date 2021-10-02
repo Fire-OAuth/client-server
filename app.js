23882b41af724ab045245eff81671eae998d8627
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-10-02 22:49:33 +0530

    Sample client server rendering login page

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 4040;
+const path = require('path')
+
+app.listen(port, () => console.log("Server Listening on " + port));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, "public/login.html"));
+})
