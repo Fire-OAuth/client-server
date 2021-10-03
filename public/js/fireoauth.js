3115c45f7b680e6df86327284f21834b22b00018
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-10-03 20:36:46 +0530

    Hardcoded API key for Local Development

--- public/js/fireoauth.js
+++ public/js/fireoauth.js
@@ -1,3 +1,5 @@
+const FIREAPI = "6159bbddf524e3b8264d43ef"
+const FIREENDPOINT = "http://localhost:3003/api/apis/generate"
 const channel = new BroadcastChannel("my_bus")
 channel.addEventListener("message", (event) => {
 	console.log(event.data)
